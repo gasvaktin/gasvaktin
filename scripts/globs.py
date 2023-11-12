b296b8069445f93bb3ac20d17a280c194321ca8f
Loknar <svefgud@gmail.com>
2023-11-12 20:20:40 +0000

    new N1 station

--- scripts/globs.py
+++ scripts/globs.py
@@ -373,6 +373,7 @@ N1_LOCATION_RELATION = {
     "Mývatn": "n1_083",
     'Skútuvogi': 'n1_084',
     'Skútuvogur': 'n1_084',
+    'Flugvellir': 'n1_085',
 }
 N1_DISCOUNTLESS_STATIONS = ['n1_011', 'n1_074', 'n1_081', 'n1_082', 'n1_079']
 
