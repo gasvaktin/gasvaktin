692b7653dd5d42ecc6c5903bf9b0a517bf29249e
Loknar <svefgud@gmail.com>
2025-06-17 23:50:41 +0000

    new N1 station, in Fiskislóð/Grandi

--- scripts/globs.py
+++ scripts/globs.py
@@ -443,8 +443,9 @@ N1_LOCATION_RELATION = {
     'Skútuvogi': 'n1_084',
     'Skútuvogur': 'n1_084',
     'Flugvellir': 'n1_085',
+    'Fiskislóð': 'n1_086',
 }
-N1_DISCOUNTLESS_STATIONS = ['n1_011', 'n1_074', 'n1_081', 'n1_082', 'n1_079']
+N1_DISCOUNTLESS_STATIONS = ['n1_011', 'n1_074', 'n1_081', 'n1_082', 'n1_079', 'n1_086']
 
 BROWSERS_UA = [
     ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
