608cf24f9cd05d591fbe1ff951fc53b5290a02d9
Loknar <svefgud@gmail.com>
2026-05-12 22:57:04 +0000

    revert Hæðarsmári discountless
    seems like they changed their mind and reverted prices for Hæðarsmári to the usual

--- scripts/globs.py
+++ scripts/globs.py
@@ -328,7 +328,7 @@ ORKAN_LOCATION_RELATION = {
 }
 ORKAN_DISCOUNTLESS_STATIONS = [
     'or_000', 'or_006', 'or_007', 'or_018', 'or_021', 'or_029', 'or_041', 'or_048', 'or_049',
-    'or_051', 'or_053', 'or_067', 'or_070', 'or_073'
+    'or_051', 'or_053', 'or_067', 'or_073'
 ]
 # Orkan has a default 10 ISK discount with Orkan keychain
 # See: https://www.orkan.is/afslaettir/
